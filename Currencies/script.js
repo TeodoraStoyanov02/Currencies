5f197828da1b37de3d727ecc88c58cda347a4827
TeodoraStoyanov02 <133811772+TeodoraStoyanov02@users.noreply.github.com>
2023-05-17 16:04:48 +0300

    Add files via upload

---
+++ Currencies/script.js
@@ -0,0 +1,23 @@
+
+const rates = {
+  USD: 1,
+  EUR: 0.84,
+  GBP: 0.72,
+  BGN: 1.81,
+  DIN: 108.29 
+};
+
+// Perform the currency conversion
+function convertCurrency() {
+  const amount = parseFloat(document.getElementById('amount').value);
+  const fromCurrency = document.getElementById('fromCurrency').value;
+  const toCurrency = document.getElementById('toCurrency').value;
+
+  const convertedAmount = amount * (rates[toCurrency] / rates[fromCurrency]);
+
+  document.getElementById('result').textContent = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+}
+
+// Attach event listener to the convert button
+const convertButton = document.getElementById('convertButton');
+convertButton.addEventListener('click', convertCurrency);
